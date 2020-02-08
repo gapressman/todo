f5459c7b378dc5f99b54bc98e9da700acabed31b
Gabe Pressman <gpressman@kbra.com>
2020-02-08 15:11:04 -0500

    setup repo

---
+++ src/components/layout.js
@@ -0,0 +1,26 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Header } from "./Header"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+      >
+        <main>{children}</main>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    <div class="container"></div>
+  </Layout>
+)
+
+export default IndexPage
