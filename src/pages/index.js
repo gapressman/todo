d65059da8caaa1e03e8021b0ff35f1c240c7a0cd
Gabe Pressman <gpressman@kbra.com>
2020-02-08 20:54:10 -0500

    feat: use Notepad in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 
+import { Notepad } from "../components/Notepad"
 import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-    <div class="container"></div>
+    <Notepad />
   </Layout>
 )
 
